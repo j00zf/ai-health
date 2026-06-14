a0326721e469aa11a844c13cc921901a85c7ccf0
Joseph Sebastian <josephsebastian282003@gmail.com>
2026-06-14 23:52:03 +0530

    removed

--- backend-api/src/routes/deviceRoutes.js
+++ backend-api/src/routes/deviceRoutes.js
@@ -11,8 +11,5 @@ const deviceController = require("../controllers/deviceController");
 router.post("/fitbit/connect", protect, deviceController.connectFitbit);
 router.get("/my-devices", protect, deviceController.getDevices);
 
-// --- Routes mapping to fitbitController.js ---
-router.post("/fitbit/sync", protect, fitbitController.syncDailyData);
-router.get("/fitbit/data", protect, fitbitController.getFitbitData);
 
 module.exports = router;
